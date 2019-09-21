372d68b75fc035325145d6282cb51acd7d95fa66
Chashchegorov Alexey <chashchegorov@gmail.com>
2019-09-21 19:19:02 +0300

    preliminary scripts to parse network packets entries

---
+++ capture.sh
@@ -0,0 +1,43 @@
+#!/usr/bin/env bash
+
+IFACE_NAME="eth0"
+OUT_FILE="${IFACE_NAME}.pcap"
+TIME_SECONDS=10
+
+usage() {
+	echo "./caprure.sh [ -i | --interface <interface_name> | default=${IFACE_NAME} ] "
+	echo "             [ -o | --out_file <file_name> | default=${OUT_FILE} ] "
+	echo "             [ -t | --time_seconds <capture time> | default=${TIME_SECONDS} ] "
+	echo "             [ -h | --help ] "
+}
+
+while [ "$1" != "" ]; do
+	case $1 in
+		-i | --interface )
+			shift
+			IFACE_NAME=$1
+			;;
+		-o | --out_file )
+			shift
+			OUT_FILE=$1
+			;;
+		-t | --time_seconds )
+			shift
+			TIME_SECONDS=$1
+			;;
+		-h | --help )
+			usage
+			;;
+		* )
+			usage
+			exit 1
+	esac
+	shift
+done
+
+CMD="tcpdump -i ${IFACE_NAME} -w ${OUT_FILE} & sleep ${TIME_SECONDS} ;pkill tcpdump"
+tcpdump -i ${IFACE_NAME} -w ${OUT_FILE} > /dev/null 2>&1 & sleep ${TIME_SECONDS} ; pkill tcpdump > /dev/null 2>&1
+if [ $? -ne 0 ]; then
+	echo execution failed: ${CMD} 
+	exit 1
+fi 
---
+++ parse.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+FILE_NAME="eth0.pcap"
+
+usage() {
+	echo "./parse.sh [ -i | -input_pcap <pcap file> | default=${FILE_NAME}"
+	echo "           [ -h | --help ]"
+}
+
+while [ "$1" != "" ]; do
+	case $1 in
+		-i | --input_pcap )
+			shift
+			FILE_NAME=$1
+			;;
+		-h | --help )
+			usage
+			exit 0
+			;;
+		* )
+			usage
+			exit 1;
+	esac
+	shift
+done
+ACCU=""	
+while read line; do
+	#echo $line
+	if [[ $line =~ length ]]; then
+		echo $ACCU
+		ACCU=$(echo $line | cut -d'.' -f1)":"
+	else
+		ACCU=${ACCU}$(echo $line | cut -d: -f2- | tr -s ' '| sed -e 's/ //g')
+	fi
+done < <(tcpdump -r ${FILE_NAME} -xx | tr -s ' ' )
